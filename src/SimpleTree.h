ecec0eca28479830e9118e118cb4776eb44d0c7c
timurwtf <argertimur@gmail.com>
2025-07-07 23:34:49 +0300

    add simple tree

--- src/SimpleTree.h
+++ src/SimpleTree.h
@@ -5,7 +5,25 @@
 template <typename T>
 struct SimpleTree {
  private:
+    Node<T>* root;
+    int size;
+
+    void erase(Node<T>* node, T value);
+    T find_by_id(int id, Node<T>* cur_node, int &pos);
+    void find_floor(T key, Node<T>* cur_node, T &local_max);
+    void find_ceiling(T key, Node<T>* cur_node, T &local_min);
+
  public:
+    SimpleTree();
+    SimpleTree(T key);
+    SimpleTree(Node<T>* root);
+    ~SimpleTree();
+
+    void insert(T value);
+    void erase(T value);
+    T find_by_id(int id);
+    T floor(T value);
+    T ceiling(T value);
 };
 
 #include "SimpleTree.cpp"
