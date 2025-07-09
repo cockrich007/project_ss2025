bc6a3052ee460cfd7b7f3547857a6fea8900d21d
cockrich007 <vladimirgridasov68@gmail.com>
2025-07-09 09:24:25 +0300

    gen1

--- data/TesterInsert.cpp
+++ data/TesterInsert.cpp
@@ -12,36 +12,57 @@ void generate_tests() {
 //    freopen("inputs/ascend_input.txt", "w", stdout);
     std::ofstream outfile;
 
-    outfile.open("D:/ss25project/data/inputs/ascend_input.txt");
-    gen.DataAscend(0, 10000);
+    //outfile.open("D:/ss25project/data/inputs/ascend_input.txt");
+    //gen.DataAscend(0, 10000);
+    //for (auto element : gen.Data) {
+    //    outfile << element << ' ';
+    //}
+    //outfile.close();
+
+    //outfile.open("D:/ss25project/data/inputs/random_input1.txt");
+    //gen.DataRandom(10000, 0, 10000);
+    //for (auto element : gen.Data) {
+    //    outfile << element << ' ';
+    //}
+    //outfile.close();
+
+    //outfile.open("D:/ss25project/data/inputs/random_input2.txt");
+    //gen.DataRandom(10000, 0, 10000);
+    //for (auto element : gen.Data) {
+    //    outfile << element << ' ';
+    //}
+    //outfile.close();
+
+    //outfile.open("D:/ss25project/data/inputs/OddEven_input_frac2_inc.txt");
+    //gen.DataOddEven(10000, 2, true);
+    //for (auto element : gen.Data) {
+    //    outfile << element << ' ';
+    //}
+    //outfile.close();
+
+    //outfile.open("D:/ss25project/data/inputs/OddEven_input_frac5_dic.txt");
+    //gen.DataOddEven(10000, 5, false);
+    //for (auto element : gen.Data) {
+    //    outfile << element << ' ';
+    //}
+    //outfile.close();
+
+    outfile.open("D:/ss25project/data/inputs/Jump_Input_inc.txt");
+    gen.DataJump(10000, true);
     for (auto element : gen.Data) {
         outfile << element << ' ';
     }
     outfile.close();
 
-    outfile.open("D:/ss25project/data/inputs/random_input1.txt");
-    gen.DataRandom(10000, 0, 10000);
+    outfile.open("D:/ss25project/data/inputs/Jump_Input_dic.txt");
+    gen.DataJump(10000, false);
     for (auto element : gen.Data) {
         outfile << element << ' ';
     }
     outfile.close();
 
-    outfile.open("D:/ss25project/data/inputs/random_input2.txt");
-    gen.DataRandom(10000, 0, 10000);
-    for (auto element : gen.Data) {
-        outfile << element << ' ';
-    }
-    outfile.close();
-
-    outfile.open("D:/ss25project/data/inputs/OddEven_input_frac2_inc.txt");
-    gen.DataOddEven(10000, 2, true);
-    for (auto element : gen.Data) {
-        outfile << element << ' ';
-    }
-    outfile.close();
-
-    outfile.open("D:/ss25project/data/inputs/OddEven_input_frac5_dic.txt");
-    gen.DataOddEven(10000, 5, false);
+    outfile.open("D:/ss25project/data/inputs/Edge_input.txt");
+    gen.DataEdge();
     for (auto element : gen.Data) {
         outfile << element << ' ';
     }
